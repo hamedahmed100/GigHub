936ac99d3e236f75e6b3d310483c7b9f00fef6c6
hamed Ahmed <hamedahmedhamed100@gmail.com>
2021-02-13 00:26:35 +0200

    move DateTime Parsing to GigFormViewModel

--- GigHub/ViewModel/GigFormViewModel.cs
+++ GigHub/ViewModel/GigFormViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GigHub.Models;
+using System;
 using System.Collections.Generic;
 
 namespace GigHub.ViewModel
@@ -11,5 +12,10 @@ namespace GigHub.ViewModel
         public byte Genre { get; set; }
 
         public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime DateTime
+        {
+            get { return DateTime.Parse($"{Date} {Time}"); }
+        }
     }
 }
